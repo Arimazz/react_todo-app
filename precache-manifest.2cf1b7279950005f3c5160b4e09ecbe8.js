5c6666b62740e8ecd4ddfdda6372c63b7485c3f1
Arimazz <bodyaprymyak@icloud.com>
2020-03-02 03:04:22 +0200

    Updates

---
+++ precache-manifest.2cf1b7279950005f3c5160b4e09ecbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f144c1fd72a0669676c6d19bb06fc127",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "ca3aeeed2dfc9e8983e1",
+    "url": "/react_todo-app/static/css/main.8031d187.chunk.css"
+  },
+  {
+    "revision": "2f00b1285adc9301085b",
+    "url": "/react_todo-app/static/js/2.415d4b66.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_todo-app/static/js/2.415d4b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3aeeed2dfc9e8983e1",
+    "url": "/react_todo-app/static/js/main.448b4c9a.chunk.js"
+  },
+  {
+    "revision": "0ae84b7e4dfb6d5ed076",
+    "url": "/react_todo-app/static/js/runtime-main.a1204df4.js"
+  }
+]);
